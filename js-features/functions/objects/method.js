138a7939b36f7d5949c84ccce4c9cab0b42acb1b
JerkoMintHome <jerome.potie@gmail.com>
2020-08-30 01:13:07 +0200

    Add new object functions and add functions to poo files

---
+++ js-features/functions/objects/method.js
@@ -0,0 +1,8 @@
+const object = {
+  //TODO method
+  sum(a,b){
+    return a + b
+  }
+}
+
+console.log(object.sum(1,2))
--- js-features/poo/base.js
+++ js-features/poo/base.js
@@ -3,17 +3,14 @@ class Person {
   //TODO Instanciation de l'objet
   constructor(identity) {
     this.firstname = identity.firstname
-    this.lastname = identity.lastname
     this.birthday = identity.birthday
-    this.language = identity.language
-    this.country = identity.country
     this.interest = identity.interest
     this.genre = identity.genre
     this.salutation = () => {
       return `Coucou, je me présente, je m'appelle ${this.firstname}`
     }
     this.bio = () => {
-      return `${this.lastname} ${this.firstname} est ${this.genre === 'homme' ? 'né' : 'née'} le ${this.birthday} et ${this.genre === 'homme' ? 'il' : 'elle'} aime les activités suivantes: ${this.interest.map(item => item)} `
+      return `${this.firstname} est ${this.genre === 'homme' ? 'né' : 'née'} le ${this.birthday} et ${this.genre === 'homme' ? 'il' : 'elle'} aime les activités suivantes: ${this.interest.map(item => item)} `
     }
   }
 }
@@ -21,30 +18,47 @@ class Person {
 //TODO définition d'une nouvelle instance
 const jerome = new Person({
   firstname: 'Jérôme',
-  lastname: 'Potié',
   genre: 'homme',
   birthday: '16/12/1977',
-  language: 'Français',
-  country: 'France',
   interest: ['randonnée', 'raquettes', 'tarot']
 })
 const clemence = new Person({
   firstname: 'Clémence',
-  lastname: 'Ryckebush',
   genre: 'femme',
   birthday: '28/08/1988',
-  language: 'Français',
-  country: 'France',
   interest: ['tarot', 'boulot', 'cuisine']
 })
 console.log(jerome)
 console.log(jerome.salutation())
 
+//TODO Lire la propriété d'un objet
+console.log(clemence['genre'])
+
 //TODO Modifier la propriété d'un objet
 clemence.birthday = '28/08/1987'
 console.log(clemence.birthday)
+
 //TODO Destructurer un tableau afin d'y ajouter une donnée
 jerome.interest = [...jerome.interest, 'guitare']
 console.log(jerome.interest)
+
 console.log(jerome.bio())
 console.log(clemence.bio())
+
+//TODO Héritage avec nouvelles propriétés
+class Teacher extends Person {
+  constructor(identity){
+    super(identity)
+    this.matter = identity.matter
+  }
+}
+
+const thomas = new Teacher({
+  firstname: 'Thomas',
+  genre: 'Homme',
+  birthday: '12/06/75',
+  interest: ['Informatique', 'Jeux Vidéos'],
+  matter: 'ReactJS'
+})
+
+console.log(thomas.salutation())
