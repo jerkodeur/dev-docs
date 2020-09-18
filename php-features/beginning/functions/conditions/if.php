ed795706db6c6b07c391a697cf090b8bac0d6fe5
JerkoMintHome <jerome.potie@gmail.com>
2020-09-18 10:29:22 +0200

    remove help function to a specific folder

---
+++ php-features/beginning/functions/conditions/if.php
@@ -0,0 +1,10 @@
+<?php
+
+//TODO if
+//* if (expression) {
+        // bloc d'instructions
+//* } elseif (expression) {
+        // bloc d'instructions
+//* } else {
+        // bloc d'instructions
+//* }
---
+++ php-features/beginning/functions/loops/while.php
@@ -0,0 +1,10 @@
+<?php
+include('service/variables.php');
+
+//TODO boucle while
+echo "<b>----- Boucle while -----</b>" . $line;
+$i = 0;
+while ($i < 10) {
+    $i++;
+    echo $i. ' ';
+}
