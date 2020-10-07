6d2023e00acc90d38221cb886f35f2c0c62d7f83
JerkoMintHome <jerome.potie@gmail.com>
2020-10-07 22:32:00 +0200

    Initialize a class to formatting the display of function results

---
+++ php-features/beginning/service/classes/display_examples.php
@@ -0,0 +1,17 @@
+<?php
+
+class formatting {
+
+    public $return = '<br />';
+    public $empty_line = '<br /><br />';
+
+    public function __construct($description, $result)
+    {
+        $this->description = $description;
+        $this->result = $result;
+    }
+    public function return_exemple(){
+
+    }
+}
+?>
