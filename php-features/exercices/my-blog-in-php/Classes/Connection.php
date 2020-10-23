c3ace24f7278cfd6bcaf80258cb353e0a2103d27
JerkoMintHome <jerome.potie@gmail.com>
2020-10-24 00:57:39 +0200

    Regroup connection needs to connection.php

--- php-features/exercices/my-blog-in-php/Classes/Connection.php
+++ php-features/exercices/my-blog-in-php/Classes/Connection.php
@@ -3,51 +3,51 @@
 namespace App;
 
 use App\Main\Debug;
-use \PDO;
+use PDO;
 
 
 /**
- * Management of database, connection and queries
- * @package App/Database
+ * Main method to connect and manage database connection
  */
+class Connection {
 
-class Database {
-
-    private $db_name;
-    private $db_host;
-    private $db_user;
-    private $db_password;
+    private $DB_HOST;
+    private $DB_USER;
+    private $DB_PASS;
+    private $DB_NAME;
     private $pdo;
 
     /**
-     * Database constructor
-     *
-     * @param string $db_name
-     * @param string $db_host
-     * @param string $db_user
-     * @param string $db_password
+     * Define variables for the database connection
+     * @param string $db_name Name of the database
+     * @return void
      */
-    public function __construct($db_name, $db_host = 'locahost', $db_user = 'root', $db_password = 'root'){
-        $this->db_name = $db_name;
-        $this->db_host = $db_host;
-        $this->db_user = $db_user;
-        $this->db_password = $db_password;
+    public function __construct($db_name)
+    {
+        $this->DB_NAME = $db_name;
+        $this->DB_HOST = env('DB_HOST');
+        $this->DB_USER = env('DB_USERNAME');
+        $this->DB_PASS = env('DB_PASSWORD');
     }
 
+    /**
+     * Open connection to database with PDO
+     * @return pdo object
+     */
     private function getPDO(){
         if($this->pdo === null){
-            $pdo = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user, $this->db_password);
+            $pdo = new PDO("mysql:host=$this->DB_HOST;dbname=$this->DB_NAME", $this->DB_USER, $this->DB_PASS);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo = $pdo;
         }
         return $this->pdo;
     }
+
     /**
      * Manage queries to the bdd
-     *
      * @param string $statement Database statement
      * @param class $class Class to load the datas
-     */
+    */
     public function query($statement, $class){
         $req = $this->getPDO()->query($statement);
         $datas = $req->fetchAll(PDO::FETCH_CLASS, $class);
@@ -56,19 +56,16 @@ class Database {
 
     /**
      * Prepare a query to avoid inject sql (security)
-     *
      * @param query $statement
      * @param string|array $params
      * @param class $class path of the called class
      * @param boolean $one Return one or many results
      * @return string|array Depending of the number of result expected
      */
-
     public function prepare($statement, $params, $class, $one = true){
         $req = $this->getPDO()->prepare($statement); // Avoid inject html
         $req->execute($params); // joins the params to the query
         $req->setFetchMode(PDO::FETCH_CLASS, $class); // Allow assigning a class to the PDO request
-        Debug::inspectElement($req);
         // Define the expect number of results
         if($one){
             return $req->fetch();
@@ -76,7 +73,6 @@ class Database {
             return $req->fetchAll();
         }
     }
-
 }
 
 ?>
