ef5d8e76801894e46c596e7eb179c035e371d550
JerkoMintHome <jerome.potie@gmail.com>
2020-11-03 22:24:28 +0100

    Modify get route for index to recover episodes list and create a new get controller route to display episodes of the selected formation

--- php-features/exercices/formation-project-with-laravel-and-vue/app/Http/Controllers/CoursesController.php
+++ php-features/exercices/formation-project-with-laravel-and-vue/app/Http/Controllers/CoursesController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use App\Models\Episode;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class CoursesController extends Controller
 {
     protected function index() {
-        $courses = Course::all();
+        $courses = Course::with('user')->withCount('episodes')->get();
 
         return Inertia::render('Courses/index', [
             'courses' => $courses
+        ]);
+    }
+
+    protected function show($course){
+        $course = Course::where('id', $course)->with('episodes')->first();
+        return Inertia::render('Courses/show', [
+            'course' => $course
         ]);
     }
 }
